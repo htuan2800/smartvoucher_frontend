a2fc5283ae556a966396d8a45f9dd3c0ff466e6a
htuan2800 <huynhngoctuan48@gmail.com>
2026-03-12 10:43:06 +0700

    up

--- src/components/layout/admin/SidebarItem.tsx
+++ src/components/layout/admin/SidebarItem.tsx
@@ -19,42 +19,10 @@ export const sidebarItems = [
     title: "Nhân viên",
     icon: <Users />,
     path: "/admin/staffs/list",  
-    requiredPermission: 'staff.view'
   },
   {
     title: "Khách hàng",
     icon: <UserRound />,
     path: "/admin/customers/list",
-    requiredPermission: 'customer.view'
-  },
-  {
-    title: "Địa điểm",
-    icon: <LocateIcon />,
-    path: "/admin/locations/list",
-    requiredPermission: 'location.view'
-  },
-  {
-    title: "Tour",
-    icon: <Torus />,
-    path: "/admin/tours/list",
-    requiredPermission: 'tour.view'
-  },
-  {
-    title: "Mã giảm giá",
-    icon: <Percent />,
-    path: "/admin/coupons/list",
-    requiredPermission: 'coupon.view'
-  },
-  {
-    title: "Bookings",
-    icon: <ListOrdered />,
-    path: "/admin/bookings/list",
-    requiredPermission: 'booking.view'
-  },
-  {
-    title: "Phân quyền",
-    icon: <Users />,
-    path: "/admin/roles/list",
-    requiredPermission: 'role.view'
   }
 ]
